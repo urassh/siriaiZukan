54b16b5524830106a1071019488e7f1751abc870
urassh <shuto.1011@icloud.com>
2023-09-21 10:25:16 +0900

    ライブラリの画像取得処理を書いた

--- siriaiZukan/Community/ViewController/CommunityEditViewController.swift
+++ siriaiZukan/Community/ViewController/CommunityEditViewController.swift
@@ -13,14 +13,18 @@ class CommunityEditViewController: UIViewController {
     @IBOutlet var iconImage: UIImageView!
     @IBOutlet var captureButton: UIButton!
     @IBOutlet var nameTextField: UITextField!
+    
+    let picker = UIImagePickerController()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         if iconImage.image != nil {
             captureButton.isHidden = true
         }
         
+        picker.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,6 +35,8 @@ class CommunityEditViewController: UIViewController {
     
     @IBAction func iconButtonTapped() {
         ///ライブラリにアクセスし、ゲットした写真をiconImageに代入する処理。
+        showAlert()
+        print("button tapped")
     }
     
     @IBAction func registerTapped() {
@@ -38,17 +44,41 @@ class CommunityEditViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-}
-
-extension UIImage {
-    func roundedCorners(radius: CGFloat) -> UIImage {
-        return UIGraphicsImageRenderer(size: self.size).image { context in
-            let rect = context.format.bounds
-            let roundedPath = UIBezierPath(roundedRect: rect,
-                                           cornerRadius: radius)
-            roundedPath.addClip()
-            // UIImageを描画
-            draw(in: rect)
+    private func showAlert() {
+        let alert = UIAlertController(title: "どの方法で画像を取得しますか？", message: "", preferredStyle: .actionSheet)
+        let cameraSheet = UIAlertAction(title: "Camera", style: .default) { (action) in
+            //ここにカメラの起動処理を書く。
+            self.dismiss(animated: true, completion: nil)
         }
+        
+        let librarySheet = UIAlertAction(title: "Library", style: .default) { [self] (action) in
+            present(picker, animated: true)
+//            self.dismiss(animated: true, completion: nil)
+        }
+        
+        let cancel = UIAlertAction(title: "キャンセル", style: .cancel) { (acrion) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        alert.addAction(cameraSheet)
+        alert.addAction(librarySheet)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
+
+extension CommunityEditViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let firstImage = info[.originalImage] as! UIImage
+        iconImage.image = firstImage
+        captureButton.isHidden = true
+        self.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+
